f776a9d8fb60c925591fa225ef5a960508767a23
“anildalar” <dollorinfotech@gmail.com>
2021-12-13 09:36:55 +0530

    v1

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f3d1ce3de66007e95801e5d9f2cc3665'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5880100c-00fe-4d7f-ab51-4fbf92574120'
+};
